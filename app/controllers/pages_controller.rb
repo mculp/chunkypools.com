702a1d58d306b9e8f81691da7b97707cf7474865
Matt Culpepper <matt@culpepper.co>
2014-02-06 23:34:54 -0600

    redirects and alerts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   before_action :login_from_php, except: [:logout]
 
   def logout
-    redirect_to chunky_mpos + "/index.php?page=logout"
+    redirect_to chunky_mpos + "/index.php?page=logout&to=/"
   end
 end
