42003a700b73c91412c6bc0f81bd6b5ac12c9481
Matt Culpepper <matt@culpepper.co>
2014-04-12 14:39:48 -0500

    sigh

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
   def article_header(article, article_counter)
     header = truncate(article.header, length: 80)
-    id = @number_of_articles ? @number_of_articles - article_counter : article_counter 
+    id = @number_of_articles ? (@number_of_articles - 1) - article_counter : article_counter 
     link_to header, "/articles/#{id}-#{header.parameterize}" 
   end
 end
