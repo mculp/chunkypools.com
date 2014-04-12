3dccc1b8fdce93a3c884f1ef9f3bf91ea414406c
Matt Culpepper <matt@culpepper.co>
2014-04-12 14:08:07 -0500

    direct article links fix

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
-  def article_header(article_counter)
+  def article_header(article, article_counter)
     header = truncate(article.header, length: 80)
     link_to header, "/articles/#{article_counter + 1}-#{header.parameterize}" 
   end
