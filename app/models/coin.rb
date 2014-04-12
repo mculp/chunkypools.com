9a29f51b8bb9417438c27aab07191d73f47a5115
Matt Culpepper <matt@culpepper.co>
2014-04-12 15:33:25 -0500

    alphabetize coin list

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -14,28 +14,18 @@ class Coin
 
   # TODO: move this to config file
   CONTAINER = [
+   Coin.new(name: 'Defcoin', code: 'def', reward: 50, port: 3349),
+   Coin.new(name: 'Digibyte', code: 'dgb', reward: 8000, port: 3340),
    Coin.new(name: 'Dogecoin', code: 'doge', reward: 250000, port: 22550, type: 'p2pool'),
+   Coin.new(name: 'Klondikecoin', code: 'kdc', reward: 8, port: 3341),
    Coin.new(name: 'Muniti', code: 'mun', reward: 39, port: 1133, algorithm: 'x11'),
-   Coin.new(name: 'Stoopidcoin', code: 'stp', reward: 25000, port: 3334),
+   Coin.new(name: 'Potcoin', code: 'pot', reward: 420, port: 3420, price_source: 'mintpal'),
+   Coin.new(name: 'Procoin', code: 'pcn', reward: 1000, port: 3347),
    Coin.new(name: 'RonPaulCoin', code: 'rpc', reward: 1, port: 3335),
    Coin.new(name: 'Rubycoin', code: 'ruby', reward: 250, port: 3342),
-   Coin.new(name: 'Digibyte', code: 'dgb', reward: 8000, port: 3340),
-   Coin.new(name: 'Klondikecoin', code: 'kdc', reward: 8, port: 3341),
-   Coin.new(name: 'Potcoin', code: 'pot', reward: 420, port: 3420, price_source: 'mintpal'),
-   Coin.new(name: 'Suncoin', code: 'sun', reward: 10, port: 3345, price_source: 'mintpal'),
-   Coin.new(name: 'Procoin', code: 'pcn', reward: 1000, port: 3347),
    Coin.new(name: 'Spartancoin', code: 'spn', reward: 150000, port: 3348),
-   Coin.new(name: 'Defcoin', code: 'def', reward: 50, port: 3349),
-   Coin.new(name: 'Auroracoin', code: 'aur', reward: 25, port: 3346, active: false),
-   Coin.new(name: 'Flappycoin', code: 'flap', reward: 500000, port: 3343, active: false),
-   Coin.new(name: 'RonSwansonCoin', code: 'ron', reward: 0.125, port: 3344, active: false),
-   Coin.new(name: 'Dogecoin', code: 'doge', reward: 250000, port: 3333, active: false),
-   Coin.new(name: 'Earthcoin', code: 'eac', reward: 11000, port: 3334, active: false),
-   Coin.new(name: 'Lottocoin', code: 'lot', reward: 32000, port: 3336, active: false),
-   Coin.new(name: 'Stablecoin', code: 'sbc', reward: 25, port: 3337, active: false),
-   Coin.new(name: '42', code: '42', reward: 0.000042, port: 3338, active: false),
-   Coin.new(name: 'Litecoin', code: 'ltc', reward: 50, port: 3340, active: false),
-   Coin.new(name: 'Leafcoin', code: 'leaf', reward: 500000, port: 3342, active: false)
+   Coin.new(name: 'Stoopidcoin', code: 'stp', reward: 25000, port: 3334),
+   Coin.new(name: 'Suncoin', code: 'sun', reward: 10, port: 3345, price_source: 'mintpal')
   ]
 
   def self.all
@@ -43,7 +33,7 @@ class Coin
   end
 
   def self.[](code)
-    CONTAINER.find { |c| c.code == code.downcase } 
+    CONTAINER.find { |c| c.code == code.downcase }
   end
 
   def self.active
@@ -51,7 +41,7 @@ class Coin
   end
 
   def self.active_mpos
-    CONTAINER.select { |c| c.active && c.type == 'mpos' } 
+    CONTAINER.select { |c| c.active && c.type == 'mpos' }
   end
 
   def self.scrypt
