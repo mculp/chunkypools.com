77b079b4ea95ce9c1ca20c3620fe298e1140e394
Matt Culpepper <matt@culpepper.co>
2014-04-17 23:52:15 -0500

    profitability api now working

---
+++ test/models/market_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class MarketTest < ActiveSupport::TestCase
+  def setup
+    @market = Market.by_name(:bittrex)
+  end
+
+  test "by_name will return the correct object" do
+    assert Market::Bittrex === @market
+  end
+
+  test "it will correctly grab the market's name" do
+    assert_equal 'bittrex', @market.name
+  end
+
+  test "it serializes in the correct format" do
+    json = @market.to_json
+    parsed_json = JSON.parse(json)
+
+    assert_equal 'bittrex', parsed_json['name']
+    assert Array === parsed_json['pairs']
+
+    first_pair = parsed_json['pairs'].first
+
+    assert first_pair['name'] =~ /BTC/
+    assert first_pair.has_key? 'volume'
+    assert first_pair.has_key? 'high'
+    assert first_pair.has_key? 'low'
+    assert first_pair['last'].present?
+  end
+end
