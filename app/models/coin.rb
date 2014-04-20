983333385f6e725d888a4420341141775e1710f3
Matt Culpepper <matt@culpepper.co>
2014-04-19 21:39:22 -0500

    add kashmir and karmacoin

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -17,16 +17,14 @@ class Coin
    Coin.new(name: 'Defcoin', code: 'def', reward: 50, port: 3349),
    Coin.new(name: 'Digibyte', code: 'dgb', reward: 8000, port: 3340),
    Coin.new(name: 'Dogecoin', code: 'doge', reward: 250000, port: 22550, type: 'p2pool'),
-   Coin.new(name: 'Jennycoin', code: 'jny', reward: 867.5309, port: 3336),
-   Coin.new(name: 'Klondikecoin', code: 'kdc', reward: 8, port: 3341),
+   Coin.new(name: 'Karmacoin', code: 'karm', reward: 110000, port: 3339),
+   Coin.new(name: 'Kashmircoin', code: 'ksc', reward: 0.001001, port: 3338),
    Coin.new(name: 'Muniti', code: 'mun', reward: 39, port: 1133, algorithm: 'x11'),
    Coin.new(name: 'Potcoin', code: 'pot', reward: 420, port: 3420, price_source: 'mintpal'),
-   Coin.new(name: 'Procoin', code: 'pcn', reward: 1000, port: 3347),
    Coin.new(name: 'RonPaulCoin', code: 'rpc', reward: 1, port: 3335),
    Coin.new(name: 'Rubycoin', code: 'ruby', reward: 250, port: 3342),
    Coin.new(name: 'Spartancoin', code: 'spn', reward: 150000, port: 3348),
    Coin.new(name: 'Stoopidcoin', code: 'stp', reward: 25000, port: 3334),
-   Coin.new(name: 'Suncoin', code: 'sun', reward: 10, port: 3345, price_source: 'mintpal'),
    Coin.new(name: 'Whitecoin', code: 'wc', reward: 30000, port: 3337)
   ]
 
