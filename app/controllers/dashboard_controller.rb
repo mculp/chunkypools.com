190ee303111291a2298fb8b0e924e63aed0c7f2f
Matt Culpepper <matt@culpepper.co>
2014-04-25 12:31:54 -0500

    ugh, show hash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,5 +6,7 @@ class DashboardController < ApplicationController
 
     @display_data = Dashboard.display_data
     @latest_display_data = Hashie::Mash[@display_data.first]
+
+    @hash = Pool.status('doge')[:hash_rate] rescue 0
   end
 end
