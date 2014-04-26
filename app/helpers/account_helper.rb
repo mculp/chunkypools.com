8a551e229dc4fcf79ec98d47d733714fff4524c7
Matt Culpepper <matt@culpepper.co>
2014-04-26 01:00:42 -0500

    handle negative balances

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,5 +1,15 @@
 module AccountHelper
   def btc_value(balance)
-    (balance[:confirmed].to_f + balance[:unconfirmed].to_f) * @exchange_rates.for(balance[:coin]).current_price 
+    (balance[:confirmed].to_f + balance[:unconfirmed].to_f) * @exchange_rates.for(balance[:coin]).current_price
+  end
+
+  def balance_display(balance)
+    if balance
+      balance = 0 if balance < 0
+
+      "%.8f" % balance
+    else
+      "err"
+    end
   end
 end
