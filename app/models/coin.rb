72f4e7cb36ea5460e0503360285fd3fc8c5f99e3
Matt Culpepper <matt@culpepper.co>
2014-04-28 19:26:50 -0500

    add back pot, ruby, rpc

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -14,18 +14,18 @@ class Coin
 
   # TODO: move this to config file
   CONTAINER = [
-   Coin.new(name: 'Bitstar', code: 'bits', reward: 124, port: 3341, price_sources: ['bittrex', 'cryptocoincharts']),
+   Coin.new(name: 'Bitstar', code: 'bits', reward: 124, active: false, port: 3341, price_sources: ['bittrex', 'cryptocoincharts']),
    Coin.new(name: 'Defcoin', code: 'def', reward: 50, port: 3349, price_sources: []),
-   Coin.new(name: 'Digibyte', code: 'dgb', reward: 8000, port: 3340, price_sources: ['mintpal']),
+   Coin.new(name: 'Digibyte', code: 'dgb', reward: 8000, active: false, port: 3340, price_sources: ['mintpal']),
    Coin.new(name: 'Dogecoin', code: 'doge', reward: 250000, port: 3333, price_sources: ['mintpal', 'bittrex']),
-   Coin.new(name: 'Karmacoin', code: 'karm', reward: 110000, port: 3339, price_sources: ['bittrex']),
-   Coin.new(name: 'Kashmircoin', code: 'ksc', reward: 0.001001, port: 3338, price_sources: ['bittrex', 'cryptocoincharts']),
-   Coin.new(name: 'Muniti', code: 'mun', reward: 39, port: 1133, price_sources: ['bittrex', 'cryptocoincharts'], algorithm: 'x11'),
+   Coin.new(name: 'Karmacoin', code: 'karm', reward: 110000, active: false, port: 3339, price_sources: ['bittrex']),
+   Coin.new(name: 'Kashmircoin', code: 'ksc', reward: 0.001001, active: false, port: 3338, price_sources: ['bittrex', 'cryptocoincharts']),
+   Coin.new(name: 'Muniti', code: 'mun', reward: 39, port: 1133, active: false, price_sources: ['bittrex', 'cryptocoincharts'], algorithm: 'x11'),
    Coin.new(name: 'Potcoin', code: 'pot', reward: 420, port: 3420, price_sources: ['mintpal', 'bittrex', 'cryptocoincharts']),
    Coin.new(name: 'RonPaulCoin', code: 'rpc', reward: 1, port: 3335, price_sources: ['cryptocoincharts'], source_attribute: :name),
    Coin.new(name: 'Rubycoin', code: 'ruby', reward: 250, port: 3342, price_sources: ['cryptocoincharts']),
-   Coin.new(name: 'Spartancoin', code: 'spn', reward: 150000, port: 3348, price_sources: ['bittrex', 'cryptocoincharts']),
-   Coin.new(name: 'Whitecoin', code: 'wc', reward: 30000, port: 3337, price_sources: ['mintpal', 'bittrex', 'cryptocoincharts'])
+   Coin.new(name: 'Spartancoin', code: 'spn', reward: 150000, port: 3348, active: false, price_sources: ['bittrex', 'cryptocoincharts']),
+   Coin.new(name: 'Whitecoin', code: 'wc', reward: 30000, port: 3337, active: false, price_sources: ['mintpal', 'bittrex', 'cryptocoincharts'])
   ]
 
   def self.all
